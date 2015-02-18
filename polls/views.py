f91ec3d0ee4f7fbfe278863f5cc6a2da35d0814b
tboz203 <tboz203@gmail.com>
2015-02-17 23:28:16 -0600

    initial version of polls app working
    added views, urls, and templates

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
 
-# Create your views here.
+from polls.models import Question
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([p.question_text for p in latest_question_list])
+#     return HttpResponse(output)
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = RequestContext(request, {
+#         'latest_question_list': latest_question_list,
+#     })
+#     return HttpResponse(template.render(context))
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list': latest_question_list }
+    return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', { 'question': question })
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question })
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s." 
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voiting on question %s." % question_id)
+
