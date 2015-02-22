711590b490c4701c79309b53a126e86076510117
tboz203 <tboz203@gmail.com>
2015-02-22 15:58:09 -0600

    added bootstrap, removed comment-escaped code blocks, added license

--- polls/urls.py
+++ polls/urls.py
@@ -3,15 +3,6 @@ from django.conf.urls import patterns, include, url
 from polls import views
 
 urlpatterns = patterns('',
-        # # eg: /polls/
-        # url(r'^$', views.index, name='index'),
-        # # eg: /polls/5/
-        # url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-        # # eg: /polls/5/results/
-        # url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
-        # # eg: /polls/5/vote/
-        # url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-
         url(r'^$', views.IndexView.as_view(), name='index'),
         url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
         url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
